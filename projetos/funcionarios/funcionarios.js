68e96ef253004a8c41f0393f72e237f0fba3d8b4
CLODOALDO ANTONIO DA SILVA <clodoaldo.asilva3@senacsp.edu.br>
2025-04-09 22:03:46 -0300

    atualização orientação a objetos

--- projetos/funcionarios/funcionarios.js
+++ projetos/funcionarios/funcionarios.js
@@ -3,6 +3,8 @@ class Funcionario {
     _numeroDeMatricula;
     _anoDeAdmicao;
     _enderecoResidencial
+    _horasPonto = new Date().getHours();
+    _minutosPonto = new Date().getMinutes();
 
     constructor(nome, admicao, matricula, endereco) {
         this._nomeCompleto = nome;
@@ -21,11 +23,38 @@ class Funcionario {
         console.log(`${this._nomeCompleto} tem ${anoAtual} anos trabalhados`)
     }
 
-    baterPonto(horarioAtual) {
-        console.log(``)
+    baterPonto() {
+        console.log(`Ponto registrado para ${this._nomeCompleto} às ${this._horasPonto}:${this._minutosPonto} hrs`);
     }
 }
 
-let salario1 = new Funcionario("Clodoaldo Anonio da Silva", 1998, 1234, "abcd");
-salario1.receberSalario(1200);
-salario1.tempoDeEmpresa(2025);
+class Gerente extends Funcionario{
+    _autorizacaoDeDespesas;
+
+    constructor(nome, admicao, matricula, endereco, autorizacao) {
+        super(nome, admicao, matricula, endereco);
+        this._autorizacaoDeDespesas = autorizacao;
+    }
+
+    autorizacaoDaGerencia(valor) {
+        if(valor > 2000){
+            console.log(`Valor acima do permitido, não autorizado`)
+        }
+    }
+}
+
+class OperadorDeCaixa extends Funcionario {
+
+}
+
+class Estagiario extends Funcionario {
+
+}
+
+let funcionario1 = new Funcionario("Clodoaldo Anonio da Silva", 1998, 1234, "abcd");
+funcionario1.receberSalario(1200);
+funcionario1.tempoDeEmpresa(2025);funcionario1.baterPonto(19);
+let gerencia = new Gerente ("Carlos Alberto Silva", 2000, 4567, "Rua Beco");
+gerencia.autorizacaoDaGerencia(3000);
+
+
